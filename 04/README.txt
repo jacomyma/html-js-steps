cb3725616767502b63552dc08b63fefaa03249b5
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2022-12-03 12:17:04 +0100

    04 JS basics

---
+++ 04/README.txt
@@ -0,0 +1,3 @@
+### JS script
+
+The JS script just waits for the DOM to load, then renders a HTML div. It's just a possible setup for a data visualization.
---
+++ 04/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+		
+		let container = document.querySelector("#viz-container");
+		container.style.width = "500px";
+		container.style.height = "250px";
+		container.style.backgroundColor = "#6080A0";
+});
